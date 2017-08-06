b6e53817b22c6c41c94c69b961b6a25d5e904dae
emreersoyy <emre.ersoy1995@gmail.com>
2017-08-06 10:27:20 +0200

    Aufgaben Uploaded

---
+++ Programmieraufgaben/Woerterzaehlen/src/aufgabe1/Woerterzaehlen.java
@@ -0,0 +1,26 @@
+package aufgabe1;
+
+
+import java.util.HashMap;
+
+
+
+public class Woerterzaehlen {
+	
+	 String text = "no no no";
+     
+     String[] words = text.split(" ");
+     HashMap <String, Integer> wordMap = new HashMap<String, Integer> ();{ // word | anzahl
+     
+     for(int i=0;i<words.length;++i){
+         if(!wordMap.containsKey(words[i])){
+             wordMap.put(words[i], 1);
+         }
+         else{
+             wordMap.put(words[i],wordMap.get(words[i])+1);
+         }
+     }
+     
+     System.out.println(wordMap);
+    }
+}
